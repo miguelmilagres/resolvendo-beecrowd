918e80ad6b711df1df408bdb07614f9513c63b3e
miguelmilagres <miguelmdemacedo@gmail.com>
2024-03-08 18:16:49 -0300

    uri1002 em go

---
+++ go/uri1002.go
@@ -0,0 +1,18 @@
+package main
+ 
+import (
+    "fmt"
+    "math"
+)
+ 
+func main() {
+
+    var raio float64
+    pi := 3.14159
+
+    fmt.Scanf("%f", &raio)
+
+    area := math.Pow(raio, 2) * pi
+
+    fmt.Printf("A=%.4f\n", area)
+}
